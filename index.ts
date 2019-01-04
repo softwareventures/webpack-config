aae4233ba5cde42ac8fd62a0ed870a3fd3462caa
Daniel Cassidy <mail@danielcassidy.me.uk>
2019-01-04 10:14:36 +0000

    Fix lint errors.

--- index.ts
+++ index.ts
@@ -4,8 +4,8 @@ export function production(destDir: string): Configuration {
     return {
         mode: "production",
         output: {
-            path: destDir,
-            filename: "index.js"
+            filename: "index.js",
+            path: destDir
         }
     };
 }
