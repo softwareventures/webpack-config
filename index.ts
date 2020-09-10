6ae14ed887a00e3dc1efd3f480183d40c259545d
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-09-10 22:13:15 +0100

    fix(css-loader): disable "namedExport"
    This feature doesn't work properly.

--- index.ts
+++ index.ts
@@ -217,7 +217,7 @@ function WebpackConfig(projectSource: WebpackConfig.ProjectSource): (env: any) =
                         mode === "development"
                             ? "[local]-[sha256:contenthash:base64:5]"
                             : vendorCssId + "[sha256:contenthash:base64:5]",
-                    namedExport: true,
+                    namedExport: false,
                     exportGlobals: false,
                     exportLocalsConvention: "camelCaseOnly"
                 }
