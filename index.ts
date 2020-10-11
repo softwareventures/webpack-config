58bbbd0693546d59a262c9437b47b90202c0c1da
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-10-11 02:54:44 +0100

    style: apply prettier code style

--- index.ts
+++ index.ts
@@ -203,7 +203,9 @@ function WebpackConfig(projectSource: WebpackConfig.ProjectSource): (env: any) =
                         "Path to HTML template may not contain an exclamation mark (`!`)"
                     );
                 }
-                htmlOptions.template = `!!${require.resolve("ejs-loader")}?{esModule:false}!${project.html.template}`;
+                htmlOptions.template = `!!${require.resolve("ejs-loader")}?{esModule:false}!${
+                    project.html.template
+                }`;
             } else {
                 htmlOptions.templateContent = (parameters: any) =>
                     `<!DOCTYPE html><html><head><title>${String(
