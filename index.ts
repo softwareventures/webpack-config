62d93a1ff56cca89b913acfb6d28782fb5c2b76a
Daniel Cassidy <mail@danielcassidy.me.uk>
2021-02-10 11:22:50 +0000

    feat(file-loader): use contenthash instead of hash to name chunks generated by file-loader
    According to webpack, this is "more effective". Good enough for me.

--- index.ts
+++ index.ts
@@ -270,8 +270,8 @@ function WebpackConfig(projectSource: WebpackConfig.ProjectSource): (env: any) =
                 esModule: true,
                 name:
                     mode === "development"
-                        ? "[path][name]-[sha256:hash:base64:8].[ext]"
-                        : "[sha256:hash:base64:8].[ext]"
+                        ? "[path][name]-[sha256:contenthash:base64:8].[ext]"
+                        : "[sha256:contenthash:base64:8].[ext]"
             }
         };
 
